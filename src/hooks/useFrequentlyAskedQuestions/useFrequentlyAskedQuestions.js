c8c86ef2034d01bd5a4aa3f5ebff124789367d26
Bernardo Dias <bernardodiasdacruz@gmail.com>
2022-03-14 23:11:05 -0300

    Get faq data sorted by order

--- src/hooks/useFrequentlyAskedQuestions/useFrequentlyAskedQuestions.js
+++ src/hooks/useFrequentlyAskedQuestions/useFrequentlyAskedQuestions.js
@@ -1,9 +1,11 @@
 import useSWR from 'swr'
+import qs from 'qs'
 
 import { ENDPOINTS } from '@lib/constants'
 
 function useFrequentlyAskedQuestions () {
-  const { data, error } = useSWR(ENDPOINTS.FAQ)
+  const queryProfile = qs.stringify({ sort: 'order' })
+  const { data, error } = useSWR(`${ENDPOINTS.FAQ}?${queryProfile}`)
 
   return {
     data: data?.data,
