0f4db60800d9ee49a745d53fdd4a1ae690887557
Bernardo Dias <bernardodiasdacruz@gmail.com>
2022-03-31 10:26:01 -0300

    Improve Log In and Sign Up form validation (#3)

--- src/components/LogInForm/LogInForm.js
+++ src/components/LogInForm/LogInForm.js
@@ -10,7 +10,7 @@ import InputLabel from '@components/InputLabel'
 import InputError from '@components/InputError'
 import Button from '@components/Button'
 
-import { ENDPOINTS } from '@lib/constants'
+import { ENDPOINTS, EMAIL_REGEX } from '@lib/constants'
 
 const LogInForm = () => {
   const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm()
@@ -39,18 +39,30 @@ const LogInForm = () => {
     >
       <InputLabel title="Email:">
         <InputField
-          register={register('identifier', { required: true })}
+          register={register('identifier', {
+            required: 'The email is required.',
+            pattern: {
+              value: EMAIL_REGEX,
+              message: 'The email must be valid.'
+            }
+          })}
           disabled={isSubmitting}
         />
-        <InputError hasError={errors.identifier}>This field is required.</InputError>
+        <InputError hasError={errors.identifier}>{errors.identifier?.message}</InputError>
       </InputLabel>
       <InputLabel title="Password:">
         <InputField
           type="password"
-          register={register('password', { required: true })}
+          register={register('password', {
+            required: 'This password is required.',
+            minLength: {
+              value: 6,
+              message: 'The password must be at least 6 characters.'
+            }
+          })}
           disabled={isSubmitting}
         />
-        <InputError hasError={errors.password}>This field is required.</InputError>
+        <InputError hasError={errors.password}>{errors.password?.message}</InputError>
       </InputLabel>
       <InputLabel>
         <Button type="submit" wide disabled={isSubmitting}>
--- src/lib/constants.js
+++ src/lib/constants.js
@@ -23,3 +23,5 @@ export const COLLECTIONS = {
   PROFILES: 'PROFILES',
   EVENTS: 'EVENTS',
 }
+
+export const EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
