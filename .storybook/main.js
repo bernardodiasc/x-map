ca4ccf47921405bd48f2c612647dd718b49c8265
Bernardo Dias <bernardodiasdacruz@gmail.com>
2022-04-18 09:15:14 -0300

    Update path aliases

--- .storybook/main.js
+++ .storybook/main.js
@@ -24,14 +24,14 @@ module.exports = {
   webpackFinal: async (config, { configType }) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@dev': path.resolve(__dirname, './dev'),
-      '@public': path.resolve(__dirname, './public'),
-      '@data': path.resolve(__dirname, './public/data'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@containers': path.resolve(__dirname, './src/containers'),
-      '@contexts': path.resolve(__dirname, './src/contexts'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@lib': path.resolve(__dirname, './src/lib'),
+      '@dev': path.resolve(__dirname, '../dev'),
+      '@public': path.resolve(__dirname, '../public'),
+      '@data': path.resolve(__dirname, '../public/data'),
+      '@components': path.resolve(__dirname, '../src/components'),
+      '@containers': path.resolve(__dirname, '../src/containers'),
+      '@contexts': path.resolve(__dirname, '../src/contexts'),
+      '@hooks': path.resolve(__dirname, '../src/hooks'),
+      '@lib': path.resolve(__dirname, '../src/lib'),
     }
     config.module.rules.push({
       test: /\.geojson$/,
